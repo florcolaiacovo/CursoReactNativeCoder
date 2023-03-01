04fddbd3eae9cb78028ed4bff4adec6a90c1341d
Florencia Colaiacovo <florcolaiacovo@gmail.com>
2023-03-01 14:04:25 -0300

    agregamos flatlist a seccion orders

--- AppCoder/src/screens/carrito/OrdersScreen.jsx
+++ AppCoder/src/screens/carrito/OrdersScreen.jsx
@@ -1,10 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, FlatList, View, Text} from 'react-native'
 import React from 'react'
 
+//data
+import { ORDERS } from '../../data/orders'
+
+//componente
+import OrderItem from '../../components/OrderItem'
+
 const OrdersScreen = () => {
+
+  const renderOrderItem = ({item}) => (
+    <OrderItem
+      item={item}
+      onDelete={() => console.log('borrar item del carrito')} />
+  )
+
   return (
     <View>
-      <Text>OrdersScreen</Text>
+      <View>
+        <Text>OrdersScren</Text>
+      </View>
+      <FlatList
+        data= {ORDERS}
+        keyExtractor= {item => item.id}
+        renderItem= {renderOrderItem} />
     </View>
   )
 }
